19db80ef08dfd5c37fff4f5fc0f792d060e802e2
BlazZz1 <blazenrrr@gmail.com>
2026-05-11 21:04:29 +0200

    skrypt połączenia z bazą danych

--- db/connect.ts
+++ db/connect.ts
@@ -1,17 +1,28 @@
-"use client"
- import mysql from "mysql2/promise";
- import dotenv from "dotenv"
+import mysql from "mysql2/promise"
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
 
- export async function dodajDoPliku()
-    {
-        dotenv.config();
+dotenv.config();
 
-        const db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-        });
-        
-        
-    }  
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post('/db/dodaj/', async(req, res) =>{
+    
+    const db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+    });
+
+    const {link, nazwa} = req.body;
+    await db.query('INSERT INTO pliki(link, nazwa_pliku) VALUES(?, ?)')
+    await db.end()
+    
+    res.json({success: true})
+})
+app.listen(3001)
+
